0e5c03a4c90518641a491cd1980f97f1374a4f2c
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-09-22 08:40:56 -0700

    Rename service tag console.generator => drupal.generator

--- src/Application.php
+++ src/Application.php
@@ -67,7 +67,7 @@ class Application extends ConsoleApplication
     private function registerGenerators()
     {
         $consoleGenerators = $this->container
-            ->findTaggedServiceIds('console.generator');
+            ->findTaggedServiceIds('drupal.generator');
 
         foreach ($consoleGenerators as $name => $tags) {
             if (!$this->container->has($name)) {
