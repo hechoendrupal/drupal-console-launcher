b6f795c2d7cf111c860cb0660b098206786cc4f6
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-10-03 07:55:58 -0400

    Tag release RC-3. (#34)

--- src/Application.php
+++ src/Application.php
@@ -20,7 +20,7 @@ class Application extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc2';
+    const VERSION = '1.0.0-rc3';
 
     public function __construct($container)
     {
