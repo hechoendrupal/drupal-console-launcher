082d6d99ebd1ac329fe33d2dbb75ab26bca0d675
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-11-02 12:42:29 -0700

    [console] Tag 1.0.0-rc7 version. (#44)

--- src/LauncherApplication.php
+++ src/LauncherApplication.php
@@ -20,7 +20,7 @@ class LauncherApplication extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc6';
+    const VERSION = '1.0.0-rc7';
 
     public function __construct($container)
     {
