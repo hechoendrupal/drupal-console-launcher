063cea254b175dd52b9bc2a6c54e4665f81d1547
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-12-22 15:37:54 -0800

    [console] Tag 1.0.0-rc12 release.

--- src/LauncherApplication.php
+++ src/LauncherApplication.php
@@ -20,7 +20,7 @@ class LauncherApplication extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc11';
+    const VERSION = '1.0.0-rc12';
 
     public function __construct($container)
     {
