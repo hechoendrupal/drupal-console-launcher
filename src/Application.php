2e76e76002881807f95bf509c6e55b5621b4979e
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-01-03 09:33:19 -0800

    [console] Tag 1.0.0-rc13 release. (#77)

--- src/Application.php
+++ src/Application.php
@@ -23,7 +23,7 @@ class Application extends CoreApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc12';
+    const VERSION = '1.0.0-rc13';
 
     /**
      * Application constructor.
