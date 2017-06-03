4eafa6f2d9dfef3ef244cc8e1dbe47cb72a30be4
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-06-03 14:05:52 -0700

    [console] Rollback include executable instead of pcntl. (#106)

--- src/Utils/Launcher.php
+++ src/Utils/Launcher.php
@@ -18,50 +18,17 @@ class Launcher
      */
     public function launch(DrupalFinder $drupalFinder)
     {
-        $composerRoot = $drupalFinder->getComposerRoot();
-        chdir($composerRoot);
-        $vendorDir = str_replace(
-            $composerRoot .'/',
-            '',
-            $drupalFinder->getVendorDir()
-        );
+        chdir($drupalFinder->getComposerRoot());
+        $drupal = $drupalFinder->getVendorDir() . '/drupal/console/bin/drupal';
 
-        $drupal = $composerRoot.'/'.$vendorDir.'/drupal/console/bin/drupal';
         if (!file_exists($drupal)) {
             return false;
         }
 
-        $this->exec($vendorDir.'/bin/drupal', $this->readArgv());
+        $drupal = realpath($drupal).'.php';
+
+        include_once $drupal;
 
         return true;
-    }
-
-    private function exec($binary, $arguments) {
-        if (function_exists("pcntl_exec") && extension_loaded('pcntl')) {
-            pcntl_exec($binary, $arguments);
-            exit(0);
-        }
-        else {
-            $cli  = $binary . ' ' . implode(' ', $arguments);
-            passthru($cli, $status_code);
-            exit($status_code);
-        }
-    }
-
-    private function readArgv()
-    {
-        $argv = $_SERVER['argv'];
-        unset($argv[0]);
-
-        foreach ($argv as $key => $value) {
-            if (substr($value, 0, 7) == "--root=") {
-                unset($argv[$key]);
-            }
-            if (substr($value, 0, 9) == "--target=") {
-                unset($argv[$key]);
-            }
-        }
-
-        return $argv;
     }
 }
