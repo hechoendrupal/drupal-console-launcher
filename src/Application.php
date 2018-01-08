df9d16b012d58cc6673faefedcf29300b28c84a3
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-01-08 11:01:04 -0800

    [console] Tag 1.4.0 release. (#146)

--- src/Application.php
+++ src/Application.php
@@ -20,7 +20,7 @@ class Application extends CoreApplication
     /**
      * @var string
      */
-    const VERSION = '1.3.2';
+    const VERSION = '1.4.0';
 
     /**
      * Application constructor.
