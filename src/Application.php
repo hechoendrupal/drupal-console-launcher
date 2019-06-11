68a2aaf019e3d8252d0edac5b77a046dadb51b1d
enzo - Eduardo Garcia <enzo@weknowinc.com>
2019-06-11 10:48:59 +1000

    Update version to 1.9.0

--- src/Application.php
+++ src/Application.php
@@ -20,7 +20,7 @@ class Application extends CoreApplication
     /**
      * @var string
      */
-    const VERSION = '1.8.0';
+    const VERSION = '1.9.0';
 
     /**
      * Application constructor.
