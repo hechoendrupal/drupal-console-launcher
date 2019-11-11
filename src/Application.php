5638d6497a49886624487958a334ae9c3c78ce0e
enzo - Eduardo Garcia <enzo@anexusit.com>
2019-11-12 07:01:38 +1100

    Update drupal console core and en dependencies to 1.9.4 (#170)
    * Update drupal console instructions
    
    * Include drupal-finder in composer
    
    * Remove drupal-finder in composer
    
    * Update version to 1.9.0
    
    * Update version to 1.9.1
    
    * Update composer for release
    
    * Update drupal console core and en dependencies to 1.9.2
    
    * Update drupal console core and en dependencies to 1.9.3
    
    * Update drupal console core and en dependencies to 1.9.4

--- src/Application.php
+++ src/Application.php
@@ -20,7 +20,7 @@ class Application extends CoreApplication
     /**
      * @var string
      */
-    const VERSION = '1.9.3';
+    const VERSION = '1.9.4';
 
     /**
      * Application constructor.
